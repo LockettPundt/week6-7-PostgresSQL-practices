15387368c731945624293580037e7eee6d6aa08a
Lockett Pundt <kalifornium@hotmail.com>
2020-02-26 12:05:49 -0500

    routes and practice with using express.

---
+++ myFirstExpress/routes/cat.js
@@ -0,0 +1,15 @@
+'use strict'
+
+const express = require('express'),
+router = express.Router();
+
+router.get('/', (req, res) => {
+    const snippet = `<h1>meow!!!!<h1>`;
+    res
+        .status(200)
+        .send(snippet)
+        .end();
+});
+
+
+module.exports = router;
---
+++ myFirstExpress/routes/index.js
@@ -0,0 +1,27 @@
+'use strict'
+const express = require('express'),
+router = express.Router();
+
+
+
+// const rootController = (req, res) => {
+//     const snippet = `<h1>hi hi hi<h1>`;
+//     res
+//         .status(200)
+//         .send(snippet)
+//         .end();
+// };
+
+
+router.get('/', (req, res) => {
+    const snippet = `<h1>Hello World<h1><br>
+    <a href="localhost:3333/dogs" target="_blank">dogs<a><br>
+    <a href="localhost:3333/cat" target="_blank">cat<a><br>
+    <a href="localhost:3333/together" target="_blank">cats and dogs<a><br>`;
+    res
+    .status(200)
+    .send(snippet)
+    .end();
+})
+
+module.exports = router;
