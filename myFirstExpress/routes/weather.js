69dfc5bba01e54f30dd00e3355ec46a3b60d72fe
Lockett Pundt <kalifornium@hotmail.com>
2020-02-27 14:07:35 -0500

    weather route.

--- myFirstExpress/routes/weather.js
+++ myFirstExpress/routes/weather.js
@@ -1,43 +1,36 @@
 'use strict'
 const express = require('express'),
-router = express.Router();
+    router = express.Router();
 const fetch = require('node-fetch')
-const dataArray = [];
-const fetchWeather = (area) => {
-    fetch(`http://api.weatherapi.com/v1/current.json?key=95929b78acb549bd9bc151519201302&q=${area}`)
-        .then(response => {
-            return response.json();
-        })
-        .then(data => {
-            console.log(data);
-            dataArray.push(`<img src="${data.current.condition.icon}"></img>`)
-            dataArray.push(`<h3>${data.location.name}<h3>`)
-            dataArray.push(`<h3>${data.location.region}</h3>`);
-            dataArray.push(`<p>${data.current.condition.text}</p>`);
-            dataArray.push(`<p>${data.current.temp_f} Degrees F<p>`);
-        })
-        .catch(err => console.log(err));
+
+const fetchWeather = async (area) => {
+    let weatherArray = [];
+    const response = await fetch(`http://api.weatherapi.com/v1/current.json?key=95929b78acb549bd9bc151519201302&q=${area}`);
+    const data = await response.json();
+    //console.log(data);
+    weatherArray.push(`<img src="${data.current.condition.icon}"></img>`);
+    weatherArray.push(`<h3>${data.location.name}<h3>`);
+    weatherArray.push(`<h3>${data.location.region}</h3>`);
+    weatherArray.push(`<p>${data.current.condition.text}</p>`);
+    weatherArray.push(`<p>${data.current.temp_f} Degrees F<p>`);
+    return weatherArray;
 }
 
+router.get('/', async (req, res) => {
+    let {
+        loc
+    } = req.query;
 
+    const prompt = `<h1>Enter a ZIP</h1>`
 
+    const weatherInfo = await fetchWeather(loc);
+    console.log(weatherInfo)
 
-
-
-
-
-router.get('/', (req, res) => {
-    let {loc} = req.query;
-
-    const prompt =  `<h1>Enter a ZIP</h1>`
-
-    fetchWeather(loc);
-   
     res.render('template', {
         locals: {
             title: 'Weather',
-            weatherInfo: !!loc ? dataArray.join('') : prompt
-           
+            weatherInfo: !!loc ? weatherInfo.join('') : prompt
+
         },
         partials: {
             partial: 'partial-weather'
